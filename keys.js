1f142ebe3ff5e8b5b54b0da718e2c5e463841a52
Claudia <claudia@Claudias-MBP.home>
2017-12-06 20:10:07 -0800

    added packages

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'aQNzOpk7XCaV3My1cWU2fmjNy',
+  consumer_secret: 'g99yDEQfuRVNwOaJTsCZNE6SXkQuF9cKFWGbZySGILTPS3RYEf',
+  access_token_key: '516705726-TQxWaezjwAIeWGxsAB3b2VSdIFo1XUPjcz44uwc4',
+  access_token_secret: 'KUrS1Z7FxPAy5NeLIPrrlmUc4eaCMnLbTgTiwaeFfL6uU',
+}
+
+module.exports = twitterKeys;
